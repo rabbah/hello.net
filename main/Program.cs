9b3f5152954c3dc0019b08681c3b0bc39eaec8ee
Rodric Rabbah <rodric@gmail.com>
2020-12-31 00:34:08 -0500

    Use MainAsync in debugger.

--- main/Program.cs
+++ main/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace dotnethelloworld
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             JObject message = new JObject();
             message.Add("name", new JValue("visual studio"));
-            JObject result = new Hello.MainClass().xMain(message);
+            Task<JObject> task = new Hello.MainClass().MainAsync(message);
+            JObject result = await task;
             Console.WriteLine(result);
         }
     }
